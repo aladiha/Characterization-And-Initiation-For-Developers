c4ed4e27f85254a9a43ba34d8c03aab58b07e8bc
som3a <som3a@DESKTOP-4718EB3>
2019-03-20 14:02:19 +0200

    privacy

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace WebApplication2.Controllers
         {
             return View();
         }
-        public ActionResult About()
+        public ActionResult Privacy()
         {
             ViewBag.Message = "Your application description page.";
 
