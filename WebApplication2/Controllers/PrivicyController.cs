0ce82c95cfb6ae7fbb99acea8f53a2068683473e
som3a <som3a@DESKTOP-4718EB3>
2019-03-20 13:23:00 +0200

    f

---
+++ WebApplication2/Controllers/PrivicyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class PrivicyController : Controller
+    {
+        // GET: Privicy
+        public ActionResult PrivacyPolicy()
+        {
+            return View();
+        }
+    }
+}
