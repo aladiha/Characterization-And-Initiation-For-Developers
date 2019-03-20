330385c7dae70f8e51ddf27d0d2385a188e90d1a
aladiha <aladiha@ac.sce.ac.il>
2019-03-20 12:54:11 +0200

    yoy

---
+++ WebApplication2/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class User
+    {
+        public string UserName { get; set; }
+
+        public string email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
