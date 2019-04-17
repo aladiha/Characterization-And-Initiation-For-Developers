4ceb4350f5829bb2f044c8e66a88477a428bf54a
aladiha <aladiha@ac.sce.ac.il>
2019-04-17 15:02:53 +0300

    aa

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -3,12 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System;
 using WebApplication2.Models;
 using WebApplication2.DAL;
-using System.Collections.Generic;
 using System.Data.Entity.Validation;
-using System.Linq;
+
 
 namespace WebApplication2.Controllers
 {
