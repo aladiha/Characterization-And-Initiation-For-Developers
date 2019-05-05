1a558e88188ccf4ba8526eff4a86e2e0f0ccc985
aladiha <aladiha@ac.sce.ac.il>
2019-05-05 20:58:47 +0300

    requirments

---
+++ WebApplication2/Controllers/MangeProjectController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class MangeProjectController : Controller
+    {
+        // GET: MangeProject
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult StartCreating()
+        {
+            return View();
+        }
+    }
+}
