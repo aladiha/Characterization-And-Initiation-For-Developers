08ad49feb7708bb05d1df11e99fc0e3d1ace1faa
aladiha <aladiha@ac.sce.ac.il>
2019-05-08 17:16:14 +0300

    aa

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace WebApplication2.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index()//start
         {
             return View();
         }
