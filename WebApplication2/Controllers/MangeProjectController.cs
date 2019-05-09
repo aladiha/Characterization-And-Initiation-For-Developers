f1d7fe178d90f12dbd4f52b00962d9c81e279d08
aladiha <aladiha@ac.sce.ac.il>
2019-05-10 00:01:43 +0300

    create file

--- WebApplication2/Controllers/MangeProjectController.cs
+++ WebApplication2/Controllers/MangeProjectController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Aspose.Words;
+
 
 namespace WebApplication2.Controllers
 {
@@ -28,9 +30,21 @@ namespace WebApplication2.Controllers
                 choises[i-1] = frm["g"+i.ToString()].ToString();
                 Console.WriteLine("{0}",choises[i - 1]);
             }
-          
+            
+
+            // Initialize a Document.
+            Document doc = new Document();
+
+            // Use a document builder to add content to the document.
+            DocumentBuilder builder = new DocumentBuilder(doc);
+            builder.Writeln("Hello World!");
+
+            String dataDir = "C:/haha.docx";
+            // Save the document to disk.
+            doc.Save(dataDir);
             return View();
         }
+
         public ActionResult EditPermistions()
         {
             string projectid = Request.QueryString.Get("projectid");
